0adbadd8c033af23400caa6c4c35fde9cbfe0cc0
Colin Tucker <colin@praxis.net.au>
2017-10-08 17:09:41 +1100

    Removed dir and path from _config.php

--- _config.php
+++ _config.php
@@ -14,13 +14,3 @@
  * @license https://opensource.org/licenses/BSD-3-Clause BSD-3-Clause
  * @link https://github.com/praxisnetau/silverware-model-filters
  */
-
-// Define Module Constants:
-
-if (!defined('SILVERWARE_MODEL_FILTERS_DIR')) {
-    define('SILVERWARE_MODEL_FILTERS_DIR', basename(__DIR__));
-}
-
-if (!defined('SILVERWARE_MODEL_FILTERS_PATH')) {
-    define('SILVERWARE_MODEL_FILTERS_PATH', realpath(__DIR__));
-}
